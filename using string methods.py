57f60aaaf91923024ee5cf0ce1ade933b910c4df
Candace Hollinger <csig1985@gmail.com>
2022-12-12 23:13:29 +0000

    updated using string methods

---
+++ using string methods.py
@@ -0,0 +1,42 @@
+
+#Calling a method on a string object using lower and upper
+my_str.lower()
+'testing'
+my_str.upper()
+'TESTING'
+
+#Creating a Capitalized string
+my_str = 'tEsTing'
+my_str.capitalize()
+'Testing'
+
+#To title case a string
+"This is a multiword String".title()
+'This Is A Multiword Sring'
+
+#To compare one string to another using the lower method
+"CandaceH@example.com".lower() == "CandaceH@example.com"
+True
+
+my_str.isascii()
+True
+my_str.islower()
+False
+my_str.isupper()
+False
+my_str.isupper()
+False
+
+#How you call a new string and calling another method onto that string
+my_str.title().istitle()
+True
+
+ #How to determine if something is written in decimal notation
+ "1".isdecimal()
+ "11".isdigit()
+ "10".isnumeric()
+ True
+ 
+ #Determining if something is alphabetical
+ "adatcdatcd".isalpha()
+ True
